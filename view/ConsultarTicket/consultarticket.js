b308c87908d99a5ffb0813d5163d0beaf115de62
dvidaalg <dvidal17gmail.com>
2024-04-08 15:00:23 -0400

    se modifica el consultarticket linea 336

--- view/ConsultarTicket/consultarticket.js
+++ view/ConsultarTicket/consultarticket.js
@@ -333,7 +333,7 @@ function listardatatable(tick_titulo,cat_id,prio_id,est_id){
                 'pdfHtml5'
                 ],
                 "ajax":{
-                    url: '../../controller/ticket.php?op=listar_filtro',
+                    url: '../../controller/ticket.php?op=filtrar_ticket',
                     type : "post",
                     dataType : "json",
                     data:{ tick_titulo:tick_titulo, cat_id:cat_id, prio_id:prio_id, est_id:est_id},
