3201a7bcad205f0c81316c569b6c57883916722d
dvidaalg <dvidal17gmail.com>
2024-04-08 12:32:02 -0400

    se modifica index de consultarticket

--- view/ConsultarTicket/index.php
+++ view/ConsultarTicket/index.php
@@ -7,6 +7,7 @@
 <head>
     <?php require_once("../MainHead/head.php");?>
 	<title>DiegoVidal</>::Consultar Ticket</title>
+	<meta name="csrf-token" content="{{ csrf_token() }}"></meta>
 </head>
 <body class="with-side-menu">
 
