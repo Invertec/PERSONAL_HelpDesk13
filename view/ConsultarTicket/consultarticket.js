a66861f4509f44a45ca3de7cbb95b797a732e9fc
Invertec <112434597+Invertec@users.noreply.github.com>
2024-04-10 14:53:08 -0400

    Update consultarticket.js

--- view/ConsultarTicket/consultarticket.js
+++ view/ConsultarTicket/consultarticket.js
@@ -333,7 +333,7 @@ function listardatatable(tick_titulo,cat_id,prio_id,est_id){
                 'pdfHtml5'
                 ],
                 "ajax":{
-                    url: 'http://localhost:80/PERSONAL_HelpDesk13/controller/ticket.php?op=listar_filtro',
+                    url: 'http://helpdesk.invertec.cl/controller/ticket.php?op=listar_filtro',
                     type : "post",
                     dataType : "json",
                     data:{ tick_titulo:tick_titulo, cat_id:cat_id, prio_id:prio_id, est_id:est_id},
